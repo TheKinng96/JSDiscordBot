9061284b67c898a30d24e8cdaedb02bf625cca52
Feng Yuan Yap <yap@corekara.co.jp>
2020-12-18 10:23:16 +0900

    hardcoded the random picking tuff

--- src/bot.js
+++ src/bot.js
@@ -4,13 +4,24 @@ const { Client } = require('discord.js');
 const client = new Client();
 const PREFIX = "$";
 
+function getRandomInt(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let chosen = [];
+
+let members = ['Azfar', 'Henry', 'Ippei', 'Shivani', 'Yoshie', 'Vitor', 'Chenrui', 'Bat'];
+let man = ['Azfar', 'Vitor', 'Ricardo', 'Bat', 'Chenrui', 'Gen', 'Shiki'];
+let woman = ['Flo', 'Yoshie', 'Karina', 'Shivani', 'Aya'];
+
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in`)
 })
 
-client.on('message', async message => {
+client.on('message', message => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
       .trim()
@@ -18,11 +29,36 @@ client.on('message', async message => {
       .split(/\s+/)
     
     if (CMD_NAME === 'chorei') {
-      if(message.author)
-      message.channel.send('yoyo')
+      let randomNum = getRandomInt(0,(members.length-1));
+      chosen.push(members[randomNum]);
+      
+      if (chosen.includes(members[randomNum])) {
+        getRandomInt(0, members.length);
+        message.channel.send('誰でしょうか');
+        setTimeout(() => {
+          message.channel.send(`Congratulation ${members[randomNum]} has been chosen!!!`)
+        }, 1000)
+        return;
+      } else {
+        message.channel.send('誰でしょうか');
+        setTimeout(() => {
+          message.channel.send(`Congratulation ${members[randomNum]} has been chosen!!!`)
+        }, 1000)
+        return;
+      }
     }
+
   }
-  
+  if (message.content.toLowerCase() === "morning") {
+    message.reply("Morning!!!")
+  }
+  if (message.content.toLowerCase() === "who is the weirdest person on adsist?") {
+    let randomNum = getRandomInt(0,(man.length-1));
+      message.channel.send('誰でしょうか');
+      setTimeout(() => {
+        message.channel.send(`${members[randomNum]} is the weirdest person!!!`)
+      }, 1000)
+  }
 })
 
 client.login(process.env.BOT_TOKEN)
